82999c52a6e1e69a1f7ebb04c600008e403e8054
Adrian Lee <adrianlee@ladrian-a01.vmware.com>
2019-04-12 18:56:24 +0800

    new changes

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const app = express()
 app.get('/', (req, res) => res.send('Hello World!'))
 
 var server = app.listen(3000, () => {
+    console.log("============")
     console.log("Listening on port " + server.address().port + "...");
+    console.log("============")
 });
 
 module.exports = server;
