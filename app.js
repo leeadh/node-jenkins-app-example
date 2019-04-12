74fc013fa4e8004d6984a99cafed492d6291ad65
Adrian Lee <adrianlee@ladrian-a01.vmware.com>
2019-04-12 19:12:15 +0800

    new file

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-var server = app.listen(3000, () => {
+var server = app.listen(4000, () => {
     console.log("============")
     console.log("Listening on port " + server.address().port + "...");
     console.log("============")
