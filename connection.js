c9233548a0d7204daca63d0314c47f0f5503ceb6
Adrian Lee <adrianlee@ladrian-a01.vmware.com>
2019-05-20 10:07:29 +0800

    changed

--- connection.js
+++ connection.js
@@ -1,17 +1,3 @@
-//--> run this if u want to run docker compose 
-//mongoose.connect('mongodb://localhost:27017/user'); 
-// --> run this for normal docker
-//mongoose.connect('mongodb://mongodb/user'); 
-
-//const mongoDbUrl = 'mongodb://127.0.0.1:27017/user'; --> to test locally on laptop
-
-/*
-var mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost:27017/user', { useMongoClient: true },function () {
-  console.log('mongodb connected')
-})
-module.exports = mongoose
-*/
 
 const mongoClient = require('mongodb').MongoClient;
 const mongoDbUrl = process.env.MONGODB_URL;
