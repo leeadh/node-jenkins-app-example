15f9881537bed607d8190edb99a048f5b5daeeae
Adrian Lee <adrianlee@ladrian-a01.vmware.com>
2019-05-24 00:03:38 +0800

    changed

--- connection.js
+++ connection.js
@@ -4,15 +4,20 @@ const mongoDbUrl = process.env.MONGODB_URL;
 //console.log(process.env.MONGODB_URL)
 let mongodb;
 
+let RECONNECT_INTERVAL = 3000
 function connect(callback){
-    mongoClient.connect(mongoDbUrl, (err, db) => {
-        console.log("checking database")
-        console.log(err)
-        console.log(db)
-        mongodb = db;
-        callback();
-    });
+      mongoClient.connect(mongoDbUrl, (err, db) => {
+        if (err) {
+          console.log("attempting to reconnect to " + mongoDbUrl)
+          setTimeout(connect.bind(this, callback), RECONNECT_INTERVAL)
+          return
+        } else {
+          mongodb = db;
+          callback();
+        }
+      });
 }
+
 function get(){
     return mongodb;
 }
