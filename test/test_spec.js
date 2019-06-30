cef8ba0e4f3a80c8995e7d7e5176a7591567406a
Adrian Lee <adrianlee@ladrian-a01.vmware.com>
2019-06-30 13:27:28 +0800

    changed readme.md

--- test/test_spec.js
+++ test/test_spec.js
@@ -7,7 +7,8 @@ var server = require("../app.js");
 describe("Test Base Page", function() {
   describe("GET /", function() {
     it("returns status code 200", function(done) {
-      request.get(base_url, function(error, response, body) {
+      request.get("http://localhost:4000/", function(error, response, body) {
+        console.log("example")
         console.log(response)
         expect(response.statusCode).toBe(200);
         done();
